3858c53fb42fafe8056cdda7234fa9fd1e6fefdb
Corey Grandmaison <cgsustainability@gmail.com>
2019-08-18 22:48:55 -0400

    Working on carousel foundation

--- public/main.js
+++ public/main.js
@@ -1,43 +1,59 @@
 const main = async () => {
-
-  const response = await fetch(
-    "https://sdg-astro-api.herokuapp.com/api/Nasa/apod"
-    );
-    const data = await response.json();
-    if (response.status === 200) {
-    console.log(data); 
-    console.log(data.hdUrl); //Need help here
-}
-}
-
-
-
-const imageMainSetup = async () => { 
-  //grabs hero image, sets to variable, qS img tag & set to the hdUrl variable
   // we want to call the API, using fetch
   // then console.lot the response
-
   
-  
-  const response = await fetch(
+  const response1 = await fetch(
     "https://sdg-astro-api.herokuapp.com/api/Nasa/apod"
     );
-    const data = await response.json();
-    if (response.status === 200) {
-    // console.log(data);
-    // console.log(data.hdUrl);
+    const data1 = await response1.json();
+    if (response1.status === 200) {
+    // console.log(data1); 
+    // console.log(data1.hdUrl); 
+    const imageSource = data1.hdUrl; 
+    // headerimage data from api
+    // document.querySelector('.hero-image').c = imageSource;    //****Not working..WHYYYYY!!! */
+    const titleSource = data1.title; 
+    // title data from api
+    document.querySelector('.image-title').textContent = titleSource;
     
-    
-    document.querySelector('.hero-image').value = data.hdUrl
-    }
+    const copyrightSource = data1.copyright 
+    //copyright data from api
+    document.querySelector('.copyright').textContent = copyrightSource;
+  } else {
+    console.log(console.error());
+  }
 
-  // return response.json()
-  // if (response.status === 200) {
-  //   const resp = await response.json()
-  //   console.log(joke)
-  //   document.querySelector('.setup').textContent = joke.setup
-  //   document.querySelector('.punchline').textContent = joke.punchline
-};
+  const response2 = await fetch(
+    'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+  );
+  const data2 = await response2.json();
+  if (response2.status === 200) {
+    console.log(data2)
+    
+    const missionName = data2[0].mission_name;
+    document.querySelector('.launch-name').textContent = missionName;
+    const missionDescription = data2[0]
+    const launchCountdown = data2[0].launch_date_utc;
+    const launchSite = data2[0].launch_site.site_name_long;
+    document.querySelector('launch-location').textContent = 
+    console.log(launchCountdown)
+  } else {
+    console.log(console.error());
+  }
+  
+}
+
+const carouselNext = () => {
+
+}
+const carouselPrevious = () => {
+  
+}
+//create a carousel 
+  const slider = document.querySelector('.slider');
+  const previous = document.querySelector('.left-arrow-button');
+  const next = document.querySelector('.right-arrow-button');
+
 
 // document.querySelector(".left-arrow-button").addEventListener("click", // -1 carousel function);
 
